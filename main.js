d3adcb694d15a52d5047b1dd4c06079d7053eafd
Oğuzhan <oguzhan.umutlu123@gmail.com>
2023-02-05 01:38:50 +0300

    fix

--- main.js
+++ main.js
@@ -296,9 +296,7 @@ addEventListener("keydown", ev => {
 addEventListener("keyup", ev => {
     delete pressingKeys[ev.key.toLowerCase()];
     if (ev.key === " ") {
-        console.log("test")
         if (lastSpace + 300 > Date.now() && player.mode === 1) {
-            console.log("a")
             player.isFlying = !player.isFlying;
             player.velocity.y = 0;
         }
